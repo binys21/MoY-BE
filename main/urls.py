5c82cd0dd21319b79475b3e8030e2b0bd3a928af
binys21 <binys1121@ewhain.net>
2024-12-09 00:00:49 +0900

    chore: main/url change

--- main/urls.py
+++ main/urls.py
@@ -4,14 +4,14 @@ from .views import *
 app_name = 'main'
 
 urlpatterns = [
-    path('main/black/<str:category>/', BlackListView.as_view()),
-    path('main/white/<str:category>/', WhiteListView.as_view()),
-    path('main/black/search', BlackPostSearchView.as_view()),
-    path('main/white/search', WhitePostSearchView.as_view()),
-    path('main/black/<int:post_id>/', BlackPostDetailView.as_view()),
-    path('main/white/<int:post_id>/', WhitePostDetailView.as_view()),
-    path('main/black/<int:post_id>/delete/', BlackPostDeleteView.as_view()),
-    path('main/white/<int:post_id>/delete/', WhitePostDeleteView.as_view()),
+    path('black/<str:category>/', BlackListView.as_view()),
+    path('white/<str:category>/', WhiteListView.as_view()),
+    path('black/search', BlackPostSearchView.as_view()),
+    path('white/search', WhitePostSearchView.as_view()),
+    path('black/<int:post_id>/', BlackPostDetailView.as_view()),
+    path('white/<int:post_id>/', WhitePostDetailView.as_view()),
+    path('black/<int:post_id>/delete/', BlackPostDeleteView.as_view()),
+    path('white/<int:post_id>/delete/', WhitePostDeleteView.as_view()),
 
 ]
 
