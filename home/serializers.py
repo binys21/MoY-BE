eec49c020410aa048843d5289e4e006bcbd5d289
julia-heo <100216331+julia-heo@users.noreply.github.com>
2024-12-11 18:38:30 +0900

    feat: 블랙/화이트 등록 구현

--- home/serializers.py
+++ home/serializers.py
@@ -10,3 +10,13 @@ class WhiteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Black
         fields = ['id', 'img', 'color', 'frame']
+
+class BlackPostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Black
+        fields = '__all__'
+
+class WhitePostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = White
+        fields = '__all__'
--- home/views.py
+++ home/views.py
@@ -8,8 +8,19 @@ from django.db.models import Count
 import random
 from django.shortcuts import get_object_or_404
 from .serializers import *
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .storages import FileUpload, s3_client
+import os
+import graduation
+
+bucket_name = getattr(graduation.settings.base, 'AWS_STORAGE_BUCKET_NAME')
 
 class BlackHomeView(APIView):
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [IsAuthenticated()]
+        return [AllowAny()]
+        
     def get(self, request):
         if request.user.is_authenticated:
             message = "로그인한 사용자 블랙 메인화면 조회 성공"
@@ -44,9 +55,55 @@ class BlackHomeView(APIView):
             }
         }, status=status.HTTP_200_OK)
     
+    def post(self, request, format=None):
+        file = request.FILES.get('img') or request.data.get('img')
+        if isinstance(file, str):
+            data = request.data.copy()
+            data['user'] = request.user.id
+            serializer = BlackPostSerializer(data=data)
+            if serializer.is_valid():
+                instance = serializer.save()
+                response_data = serializer.data 
+                response_data['nickname'] = request.user.nickname 
+                return Response({
+                    "message": "블랙 등록 성공(이미지 링크)",
+                    "data": response_data
+                }, status=status.HTTP_201_CREATED)
+        data = request.data.copy()
+        data.pop('img')
+        data['user'] = request.user.id
+        data['img'] = "tmp"
+        serializer = BlackPostSerializer(data=data)
+        if serializer.is_valid():
+            instance = serializer.save()
+
+            # S3에 파일 업로드
+            _, ext = os.path.splitext(file.name)  # 확장자 추출
+            folder = f"{request.user.id}_{request.user.username}_img/black/{instance.id}{ext}"
+            file_url = FileUpload(s3_client).upload(file, folder)
+
+            instance.img = file_url
+            instance.save()
+
+            response_data = serializer.data 
+            response_data['nickname'] = request.user.nickname 
+            
+            return Response({
+                    "message": "블랙 등록 성공(이미지 파일)",
+                    "data": response_data
+                }, status=status.HTTP_201_CREATED)
+        return Response({
+            "message": "블랙 등록 실패",
+            "error": serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 class WhiteHomeView(APIView):
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [IsAuthenticated()]
+        return [AllowAny()]
     def get(self, request):
         if request.user.is_authenticated:
             message = "로그인한 사용자 화이트 메인화면 조회 성공"
@@ -80,4 +137,44 @@ class WhiteHomeView(APIView):
                 "posts": serializer.data
             }
         }, status=status.HTTP_200_OK)
-    
+    def post(self, request, format=None):
+        file = request.FILES.get('img') or request.data.get('img')
+        if isinstance(file, str):
+            data = request.data.copy()
+            data['user'] = request.user.id
+            serializer = WhitePostSerializer(data=data)
+            if serializer.is_valid():
+                instance = serializer.save()
+                response_data = serializer.data 
+                response_data['nickname'] = request.user.nickname 
+                return Response({
+                    "message": "화이트 등록 성공(이미지 링크)",
+                    "data": response_data
+                }, status=status.HTTP_201_CREATED)
+        data = request.data.copy()
+        data.pop('img')
+        data['user'] = request.user.id
+        data['img'] = "tmp"
+        serializer = WhitePostSerializer(data=data)
+        if serializer.is_valid():
+            instance = serializer.save()
+
+            # S3에 파일 업로드
+            _, ext = os.path.splitext(file.name)  # 확장자 추출
+            folder = f"{request.user.id}_{request.user.username}_img/white/{instance.id}{ext}"
+            file_url = FileUpload(s3_client).upload(file, folder)
+
+            instance.img = file_url
+            instance.save()
+
+            response_data = serializer.data 
+            response_data['nickname'] = request.user.nickname 
+            
+            return Response({
+                    "message": "화이트 등록 성공(이미지 파일)",
+                    "data": response_data
+                }, status=status.HTTP_201_CREATED)
+        return Response({
+            "message": "화이트 등록 실패",
+            "error": serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
