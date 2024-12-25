f229015e55a154c10b37db291c46e2f1cdd14291
julia-heo <100216331+julia-heo@users.noreply.github.com>
2024-12-26 05:56:21 +0900

    fix: 이미지 방향 유지

--- home/utils.py
+++ home/utils.py
@@ -1,8 +1,9 @@
-from PIL import Image as pil
+from PIL import Image as pil, ImageOps
 import os
 
 def rescale(image, width=1440, max_file_size=2 * 1024 * 1024):
     img = pil.open(image)
+    img = ImageOps.exif_transpose(img)
     src_width, src_height = img.size
     
     # 원본 파일 크기 확인
@@ -11,9 +12,9 @@ def rescale(image, width=1440, max_file_size=2 * 1024 * 1024):
     else:
         file_size = os.path.getsize(image.name)  # 일반 파일
 
-    # 가로, 세로 중 더 짧은 쪽을 기준으로 리사이즈 조건 확인
+    # 파일 크기나 해상도 조건 확인
     if file_size > max_file_size or min(src_width, src_height) > width:
-        # 가로 세로 중 짧은 쪽을 기준으로 비율 계산
+        # 비율 계산: 더 긴 축을 기준으로 맞추기
         if src_width > src_height:
             dst_width = width
             dst_height = int((src_height / src_width) * dst_width)  # 세로 비율 유지
@@ -22,15 +23,13 @@ def rescale(image, width=1440, max_file_size=2 * 1024 * 1024):
             dst_width = int((src_width / src_height) * dst_height)  # 가로 비율 유지
 
         # 리사이즈 수행
-        size = (dst_width, dst_height)
-        img.thumbnail(size, pil.LANCZOS)
-    img = img.convert("RGB")
-        
+        img = img.resize((dst_width, dst_height), pil.LANCZOS)  # 비율 유지하며 리사이즈
+    img = img.convert("RGB")  # JPEG로 저장 가능하게 RGB로 변환
+
     # 임시 파일 저장
     temp_dir = "./home/temp"
-    # os.makedirs(temp_dir, exist_ok=True)  # 임시 폴더가 없으면 생성
+    os.makedirs(temp_dir, exist_ok=True)  # 임시 폴더가 없으면 생성
     temp_file_path = os.path.join(temp_dir, image.name if hasattr(image, 'name') else "temp_image.jpg")
 
     img.save(temp_file_path, format="JPEG")
     return temp_file_path
-
